aa8fdbaacb0e4a8046d21d75f4cfbfa0bf86ae9f
Umchristelle <umutonichristella17@gmail.com>
2026-02-14 16:28:05 +0200

    Implemented Tag Cloud filtering and dynamic heading change

--- script.js
+++ script.js
@@ -1,3 +1,23 @@
+const tags = document.querySelectorAll(".tag");
+const postsContainer = document.querySelector(".posts");
+const heroHeading = document.querySelector(".hero h2");
+
+tags.forEach(tag => {
+    tag.addEventListener("click", function() {
+        const selectedTag = tag.innerText; 
+
+        heroHeading.innerText = `Posts about ${selectedTag}`;
+
+        posts.forEach(post => {
+            if(post.dataset.tag === selectedTag){
+                post.style.display = "block"; 
+            } else {
+                post.style.display = "none"; 
+            }
+        });
+    });
+});
+
 const searchInput = document.getElementById("searchInput");
 const posts = document.querySelectorAll(".post");
 searchInput.addEventListener("input", function() {
